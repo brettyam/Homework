f88d37ff162d812bfd6793e59f4560315ca29f1d
Brett Yamada <brett.yamada@gmail.com>
2013-11-05 21:07:17 -0800

    Homework-3.0 as of 11/5/13. Still need to validate HMTL and comment javascript along with reduce some redundancy. Functionality is pretty spot on.

--- js/renderMenu.js
+++ js/renderMenu.js
@@ -1,25 +1,36 @@
 //renderMenu.js
 
 //document on ready function
-$(function (){
+$(function(){
 	renderPizzas(com.dawgpizza.menu.pizzas);
 	renderDrinks(com.dawgpizza.menu.drinks);
 	renderDesserts(com.dawgpizza.menu.desserts);
 });
 
 function renderPizzas(pizzaItems) {
-	var pizzaTemplate = $('.pizza-template');
-	var pizzaMenu = $('.pizza-menu');
+	var meatPizzaTemplate = $('.meat-pizza-template');
+	var veggiePizzaTemplate = $('.veggie-pizza-template');
+	var meatPizzaMenu = $('.meat-pizza-menu');
+	var veggiePizzaMenu = $('.veggie-pizza-menu');
 	var pizzaInstance;
 	var priceString;
 
+	//for each item in com.dawgpizza.menu.pizzas, merge them with
+	//the template and add it to the page
 	$.each(pizzaItems, function(){
-		pizzaInstance = pizzaTemplate.clone();
+		if (!this.vegetarian) {
+			pizzaInstance = meatPizzaTemplate.clone();
+		} else {
+			pizzaInstance = veggiePizzaTemplate.clone();
+		}
 		priceString = "$" + this.prices[0] + "/$" + this.prices[1] + "/$" + this.prices[2];
-		pizzaInstance.find('.name').html(this.name + ".........." + priceString);
+		pizzaInstance.find('.name').html(this.name + "...................." + priceString);
 		pizzaInstance.find('.description').html(this.description);
-		pizzaInstance.removeClass('pizza-template');
-		pizzaMenu.append(pizzaInstance);
+		if (!this.vegetarian) {
+			meatPizzaMenu.append(pizzaInstance);
+		} else {
+			veggiePizzaMenu.append(pizzaInstance);
+		}
 	});
 }
 
@@ -28,10 +39,11 @@ function renderDrinks(drinkItems) {
 	var drinkMenu = $('.drink-menu');
 	var drinkInstance;
 
+	//for each item in com.dawgpizza.menu.drinks, merge them with
+	//the template and add it to the page
 	$.each(drinkItems, function(){
 		drinkInstance = drinkTemplate.clone();
-		drinkInstance.find('.name').html(this.name + "..........$" + this.price);
-		drinkInstance.removeClass('drink-template');
+		drinkInstance.find('.name').html(this.name + "....................$" + this.price);
 		drinkMenu.append(drinkInstance);
 	});
 }
@@ -41,10 +53,11 @@ function renderDesserts(dessertItems) {
 	var dessertMenu = $('.dessert-menu');
 	var dessertInstance;
 
+	//for each item in com.dawgpizza.menu.desserts, merge them with
+	//the template and add it to the page
 	$.each(dessertItems, function(){
 		dessertInstance = dessertTemplate.clone();
-		dessertInstance.find('.name').html(this.name + "..........$" + this.price);
-		dessertInstance.removeClass('dessert-template');
+		dessertInstance.find('.name').html(this.name + "....................$" + this.price);
 		dessertMenu.append(dessertInstance);
 	});
 }
